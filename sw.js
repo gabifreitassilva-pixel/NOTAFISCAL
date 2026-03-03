210944b06e9043e5c6f39dc2fcf003c5a7b75a21
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-03-02 21:39:54 -0300

    Update service worker for new cache and assets

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-const CACHE_NAME = 'shopee-manager-v1';
-const ASSETS = [
-  'gerenciador_shopee_v3.html',
-  'manifest.json',
+const CACHE_NAME = 'shopee-app-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
